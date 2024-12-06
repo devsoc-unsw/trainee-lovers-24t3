2ec605fb5b6aa34034275595aaee09a5e9f9c05b
AngelinFrancis <angelin.francis98@gmail.com>
2024-12-06 15:33:17 +1100

    Implemented nameTag component

--- frontend/src/app/components/NameTag/NameTag.jsx
+++ frontend/src/app/components/NameTag/NameTag.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export default function NameTag() {
+export default function NameTag({ name }) {
   return (
-    <div>NameTag</div>
+    <div className='flex items-center justify-center bg-[#01136F] text-white rounded-lg w-20 h-12 rounded-3xl text-2xl px-14'>
+      {name}
+    </div>
   )
 }
---
+++ frontend/src/app/lobby/page.jsx
@@ -0,0 +1,12 @@
+import NameTag from '../components/NameTag/NameTag'
+const Page = () => {
+  return (
+    <div className='flex flex-row w-full h-screen bg-[white] items-center justify-center gap-3'>
+      <NameTag name='Jerry' />
+      <NameTag name='Angelin' />
+      <NameTag name='Agus' />
+    </div>
+  )
+}
+
+export default Page;
