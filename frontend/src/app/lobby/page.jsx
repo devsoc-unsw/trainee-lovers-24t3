f9cb14b377de434ff34e2353eb0ec854f220531f
AngelinFrancis <angelin.francis98@gmail.com>
2024-12-06 19:33:17 +1100

    Added decorative background

--- frontend/src/app/lobby/page.jsx
+++ frontend/src/app/lobby/page.jsx
@@ -1,11 +1,15 @@
 import NameTag from '../../components/NameTag';
+import DecorativeShapesBackground from '@/components/DecorativeShapesBackground';
 
 const Page = () => {
   return (
-    <div className='flex flex-row w-full h-screen bg-[white] items-center justify-center gap-3'>
-      <NameTag name='Jerry' />
-      <NameTag name='Angelin' />
-      <NameTag name='Agus' />
+    <div className='flex flex-row w-full h-screen bg-white items-center justify-center'>
+      <DecorativeShapesBackground />
+      <div className='absolute inset-0 flex justify-center items-center gap-3'>
+        <NameTag name='Jerry' />
+        <NameTag name='Angelin' />
+        <NameTag name='Agus' />
+      </div>
     </div>
   )
 }
