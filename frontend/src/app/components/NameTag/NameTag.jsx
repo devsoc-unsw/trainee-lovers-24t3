d5d476f26fd2b1c52dead5aa82542e6bf1b5710b
AngelinFrancis <angelin.francis98@gmail.com>
2024-12-06 15:40:09 +1100

    Fixed nameTag style

--- frontend/src/app/components/NameTag/NameTag.jsx
+++ frontend/src/app/components/NameTag/NameTag.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function NameTag({ name }) {
   return (
-    <div className='flex items-center justify-center bg-[#01136F] text-white rounded-lg w-20 h-12 rounded-3xl text-2xl px-14'>
+    <div className='flex items-center justify-center bg-[#01136F] text-white w-16 h-12 rounded-3xl text-2xl px-12'>
       {name}
     </div>
   )
