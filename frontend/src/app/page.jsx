2509b370e427c876ca0386c5b85e05af037783c3
kj4c <lowkhyejac@gmail.com>
2024-12-08 01:20:40 +1100

    moved homepage to be root

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,7 +1,38 @@
-export default function Home() {
+"use client";
+
+import React, { useRef, useEffect, useState } from 'react'
+import PrimaryButton from '../components/PrimaryButton';
+import SecondaryButton from '../components/SecondaryButton';
+import StartGameModal from '../components/StartGameModal';
+import useAuthStore from '../store/useAuthStore';
+import DecorativeShapesBackground from '@/components/DecorativeShapesBackground';
+import EnterNameModal from '@/components/EnterNameModal';
+
+export default function Page() {
+  const { isHost, hasPickedRole } = useAuthStore();
+
   return (
-    <div className='flex w-full h-screen bg-white text-black text-3xl justify-center items-center'>
-      hello
+    <div className='flex relative flex-col w-full h-screen bg-white items-center justify-center gap-5'>
+    <DecorativeShapesBackground />
+    <div className='absolute inset-0 flex items-center justify-center'>
+      { (isHost && hasPickedRole) ? (
+        <StartGameModal />
+      ) : hasPickedRole ? (
+        <EnterNameModal />
+      ) : (
+        <div className='flex flex-col w-full md:w-1/3 gap-2 items-center justify-center'>
+          <PrimaryButton 
+            name="HOST"
+            action='createRoom'
+          />
+          <SecondaryButton 
+            name="JOIN"
+            action='joinRoom'
+          />
+        </div>
+     )}
     </div>
-  );
+        
+    </div>
+  )
 }
