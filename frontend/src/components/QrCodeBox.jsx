2fdae0b0d7cc167bfaaaf30de4de7c53a7009bcf
AngelinFrancis <angelin.francis98@gmail.com>
2024-12-07 23:12:01 +1100

    Added lobbyComponents

---
+++ frontend/src/components/QrCodeBox.jsx
@@ -0,0 +1,11 @@
+const QrCodeBox = () => {
+  return (
+    <img 
+      src='https://upload.wikimedia.org/wikipedia/commons/d/d0/QR_code_for_mobile_English_Wikipedia.svg'
+      alt="qr-code"
+      className='flex bg-white rounded-3xl w-1/2 md:w-1/5 shadow-md shadow-gray-400 mb-2'
+    />
+  )
+}
+
+export default QrCodeBox;
