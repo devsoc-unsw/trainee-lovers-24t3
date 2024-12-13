ba1137c7d5b7a434caf76e8f23cf3b5ceeae545e
Khye Jac <lowkhyejac@gmail.com>
2024-12-14 01:23:36 +1100

    modified to make it named roomCode, fixed some atQuestion errors

--- backend/schema/GameSession.js
+++ backend/schema/GameSession.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const GameSessionSchema = new mongoose.Schema({
-    gameId: { type: String, required: true },
+    roomCode: { type: String, required: true },
     users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'GameUser' }], 
     isActive: { type: Boolean, default: true },
     questions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Question' }], 
     votingSessions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'VotingSession' }],
-    atQuestion: {type: Int, default: 0}
+    atQuestion: {type: Number, default: 0}
 });
 
 module.exports = mongoose.model('GameSession', GameSessionSchema);
