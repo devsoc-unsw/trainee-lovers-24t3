b1b95c953a80d536b4559f39e66c505ba1a9be7a
Gede Agus Sudarmawan <z5490930@ad.unsw.edu.au>
2024-12-14 21:16:37 +0800

    removed unused code

--- frontend/src/components/PrimaryButton.jsx
+++ frontend/src/components/PrimaryButton.jsx
@@ -12,6 +12,7 @@ export default function PrimaryButton({ name, action }) {
     isHost,
     username,
     questionsSelected,
+    questionsRes,
     setIsHost,
     setShowEnterNameModal,
     setShowSelectQuestionsModal,
--- frontend/src/components/QuestionBox.jsx
+++ frontend/src/components/QuestionBox.jsx
@@ -3,8 +3,6 @@ import React, { useState } from "react";
 
 export default function QuestionBox({ question, key }) {
   // const [answer, setAnswer] = useState("");
-
-  const { questionsRes, setQuestionsRes } = useAuthStore();
   return (
     <div className="bg-[#A1CDF7] w-[350px] h-[120px] rounded-[30px] content-center text-center shadow-md shadow-[#01136F] p-2 font-mouse">
       <div className="text-3xl text-[#01136F]">
@@ -18,12 +16,6 @@ export default function QuestionBox({ question, key }) {
           label="Outlined"
           variant="outlined"
           className="rounded-[30px] w-[290px] h-[43px] px-2 mt-2 text-center text-xl"
-          required
-          onChange={(event) => {
-            const copy = [...questionsRes];
-            copy[key] = event.target.value;
-            setQuestionsRes(copy);
-          }}
         />
       </div>
     </div>
