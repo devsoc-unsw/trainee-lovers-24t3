e9e86d7aa5c8fa6758a38fa4f8f4f1a856dac679
Gede Agus Sudarmawan <z5490930@ad.unsw.edu.au>
2024-12-14 15:10:29 +0800

    Removed custom manual question

--- frontend/src/components/SelectQuestionsModal.jsx
+++ frontend/src/components/SelectQuestionsModal.jsx
@@ -3,39 +3,37 @@ import useAuthStore from "@/store/useAuthStore";
 import PrimaryButton from "@/components/PrimaryButton";
 import SecondaryButton from "@/components/SecondaryButton";
 import { useEffect, useState } from "react";
+import questionsFriendLevel from "../../../backend/friend-questions.json";
+import questionCFLevel from "../../../backend/cf-questions.json";
 
 const num = [5, 6, 7, 8, 9, 10];
 const type = ["custom", "random", "friend level", "close friend level"];
-const questions = [
-  { question: "question1", level: "friend" },
-  { question: "question2", level: "friend" },
-  { question: "question3", level: "friend" },
-  { question: "question4", level: "friend" },
-  { question: "question5", level: "friend" },
-  { question: "question6", level: "friend" },
-  { question: "question7", level: "friend" },
-  { question: "question8 long", level: "friend" },
-  { question: "question9 but make it longer", level: "friend" },
-  { question: "Whats ur name?", level: "friend" },
-  { question: "question1", level: "close friend" },
-  { question: "question2", level: "close friend" },
-  { question: "question3", level: "close friend" },
-  { question: "question4", level: "close friend" },
-  { question: "question5", level: "close friend" },
-  { question: "question6", level: "close friend" },
-  { question: "question7", level: "close friend" },
-  { question: "question8", level: "close friend" },
-  { question: "question9", level: "close friend" },
-  { question: "question10", level: "close friend" },
-];
+
+const generateDefaultQuestions = () => {
+  const questions = [];
+  questionsFriendLevel.questions.map((el) => {
+    questions.push({
+      question: el.question,
+      level: "friend level",
+    });
+  });
+
+  questionCFLevel.questions.map((el) => {
+    questions.push({
+      question: el.question,
+      level: "close friend level",
+    });
+  });
+
+  return questions;
+};
+
+const questions = generateDefaultQuestions();
 
 const SelectQuestionsModal = () => {
   const [numQuestion, setNumQuestion] = useState(5);
   const [questionsType, setQuestionsType] = useState("custom");
   const [questionList, setQuestionList] = useState([]);
-  const [isEdit, setEdit] = useState(
-    Array.from(Array(questions.length + 1).map((e) => false))
-  );
 
   const getRandomInt = (max) => {
     return Math.floor(Math.random() * max);
@@ -51,16 +49,11 @@ const SelectQuestionsModal = () => {
         questionsList.push(copy[ran]);
         copy.splice(ran, 1);
       }
-    } else if (questionsType === "friend level") {
-      const copy = questions.filter((val) => val.level === "friend");
-
-      for (let i = 0; i < numQuestion; i++) {
-        let ran = getRandomInt(copy.length);
-        questionsList.push(copy[ran]);
-        copy.splice(ran, 1);
-      }
-    } else if (questionsType === "close friend level") {
-      const copy = questions.filter((val) => val.level === "close friend");
+    } else if (
+      questionsType === "friend level" ||
+      questionsType === "close friend level"
+    ) {
+      const copy = questions.filter((val) => val.level === questionsType);
 
       for (let i = 0; i < numQuestion; i++) {
         let ran = getRandomInt(copy.length);
@@ -92,93 +85,34 @@ const SelectQuestionsModal = () => {
       setQuestionList(copy);
     };
 
-    const handleAddNewQuestion = (str) => {
-      const copy = [...isEdit];
-      copy[questions.length] = false;
-      setEdit(copy);
-      if (!str) return;
-      questions.unshift({ question: str, level: undefined });
-      handleClickQuestion(0);
-    };
-
-    const handleEditQuestion = (str, index) => {
-      const copy = [...isEdit];
-      copy[index] = false;
-      setEdit(copy);
-      if (!str) return;
-      questions[index].question = str;
-      handleClickQuestion(index);
-    };
-
     return (
       <div
         className="w-full flex flex-row gap-2 overflow-auto"
         key="question-type-list"
       >
-        {questionsType === "custom" ? (
-          <div
-            className="flex items-center justify-center text-2xl lg:text-5xl text-white bg-blue-900 font-mouse rounded-md z-10 cursor-pointer text-nowrap"
-            onClick={() => {
-              const copy = [...isEdit];
-              copy[questions.length] = true;
-              setEdit(copy);
-            }}
-            onBlur={() => {
-              const copy = [...isEdit];
-              copy[questions.length] = false;
-              setEdit(copy);
-            }}
-          >
-            {isEdit[questions.length] ? (
-              <input
-                className="flex items-center justify-center p-2 text-2xl lg:text-5xl text-white bg-mid-blue font-mouse rounded-md z-10 cursor-pointer text-nowrap"
-                type="text"
-                placeholder="insert your question..."
-                onKeyDown={(e) => {
-                  if (e.key === "Enter") handleAddNewQuestion(e.target.value);
-                }}
-              ></input>
-            ) : (
-              <p className="m-2">+ Add Question</p>
-            )}
-          </div>
-        ) : (
-          ""
-        )}
-        {questions.map((el, i) => (
-          <div
-            className={
-              questionList.includes(el)
-                ? "flex items-center justify-center text-2xl lg:text-5xl text-white bg-mid-blue font-mouse rounded-md z-10 cursor-pointer text-nowrap"
-                : "flex items-center justify-center text-2xl lg:text-5xl text-mid-blue bg-white border-[1px] border-mid-blue font-mouse rounded-md z-10 cursor-pointer text-nowrap"
-            }
-            key={`${el.question + i}`}
-            onClick={() => handleClickQuestion(i)}
-            onDoubleClick={() => {
-              const copy = [...isEdit];
-              copy[i] = true;
-              setEdit(copy);
-            }}
-            onBlur={() => {
-              const copy = [...isEdit];
-              copy[i] = false;
-              setEdit(copy);
-            }}
-          >
-            {isEdit[i] ? (
-              <input
-                className="flex items-center justify-center p-2 text-2xl lg:text-5xl text-white bg-mid-blue font-mouse rounded-md z-10 cursor-pointer text-nowrap"
-                type="text"
-                placeholder="insert your question here..."
-                onKeyDown={(e) => {
-                  if (e.key === "Enter") handleEditQuestion(e.target.value, i);
-                }}
-              ></input>
-            ) : (
+        {questions.map((el, i) => {
+          if (
+            (questionsType === "friend level" ||
+              questionsType === "close friend level") &&
+            questionsType !== el.level
+          ) {
+            return;
+          }
+
+          return (
+            <div
+              className={
+                questionList.includes(el)
+                  ? "flex items-center justify-center text-2xl lg:text-5xl text-white bg-mid-blue font-mouse rounded-md z-10 cursor-pointer text-nowrap"
+                  : "flex items-center justify-center text-2xl lg:text-5xl text-mid-blue bg-white border-[1px] border-mid-blue font-mouse rounded-md z-10 cursor-pointer text-nowrap"
+              }
+              key={`${el.question + i}`}
+              onClick={() => handleClickQuestion(i)}
+            >
               <p className="m-2">{el.question}</p>
-            )}
-          </div>
-        ))}
+            </div>
+          );
+        })}
       </div>
     );
   };
@@ -215,15 +149,15 @@ const SelectQuestionsModal = () => {
           Select Question Level
         </p>
         <div
-          className="flex flex-row items-center justify-center gap-2 w-full"
+          className="h-full flex flex-row items-center justify-center gap-2 w-full"
           key="question-type-list"
         >
           {type.map((el) => (
             <div
               className={
                 questionsType === el
-                  ? "flex items-center justify-center w-full h-full text-center py-2 text-2xl lg:text-5xl text-white bg-mid-blue font-mouse rounded-md z-10 cursor-pointer"
-                  : "flex items-center justify-center w-full h-full text-center py-2 text-2xl lg:text-5xl text-mid-blue bg-white border-[1px] border-mid-blue font-mouse rounded-md z-10 cursor-pointer"
+                  ? "flex items-center justify-center w-full h-[100px] text-center py-2 text-2xl lg:text-5xl text-white bg-mid-blue font-mouse rounded-md z-10 cursor-pointer"
+                  : "flex items-center justify-center w-full h-[100px] text-center py-2 text-2xl lg:text-5xl text-mid-blue bg-white border-[1px] border-mid-blue font-mouse rounded-md z-10 cursor-pointer"
               }
               onClick={() => {
                 selectQuestion();
